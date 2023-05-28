8d4a77108b4c0cfea4539875df8ee79780f22cf9
Abir-97 <mhabirkhan97@gmail.com>
2023-05-28 23:30:46 +0600

    File added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularWithNode';
+
+ /**
+  *
+  */
+ fileName=''
+ constructor(private http: HttpClient) {
+
+ }
+
+  onFileSelected(event:any) {
+
+    const file:File = event.target.files[0];
+     console.log(file);
+    if (file) {
+        this.fileName = file.name;
+        const formData = new FormData();
+        formData.append("thumbnail", file);
+       //  const upload$ = this.http.post("/api/thumbnail-upload", formData);
+         //upload$.subscribe();
+    }
+
+   // if (typeof (FileReader) !== 'undefined') {
+      // let reader = new FileReader();
+      //  reader.onload = (e: any) => {
+      //    this.fileName = e.target.result;
+      //  };
+
+      //  reader.readAsArrayBuffer(event.target.files[0]);
+     //}
+
+     if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event:any) => { // called once readAsDataURL is completed
+        this.fileName = event.target.result;
+      }
+    }
+}
 }
